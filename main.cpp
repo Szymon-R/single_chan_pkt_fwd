ccb5379d6d39a679364d6b4246bc70d0abe03818
Szymon-R <roskwitalskiszymon@gmail.com>
2019-09-02 20:40:54 +0100

    Changing TTN adress

--- main.cpp
+++ main.cpp
@@ -72,7 +72,6 @@ sf_t sf = SF7;
 
 // Set center frequency
 uint32_t  freq = 868100000; // in Mhz! (868.1)
-
 // Set location
 float lat=0.0;
 float lon=0.0;
@@ -85,7 +84,7 @@ static char description[64] = "";                        /* used for free form d
 
 // define servers
 // TODO: use host names and dns
-#define SERVER1 "54.72.145.119"    // The Things Network: croft.thethings.girovito.nl
+#define SERVER1 "52.169.76.203"    // The Things Network: croft.thethings.girovito.nl
 //#define SERVER2 "192.168.1.10"      // local
 #define PORT 1700                   // The port on which to send data
 
